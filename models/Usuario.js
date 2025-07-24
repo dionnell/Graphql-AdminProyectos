fb247bc697a3b6609c3441be944def3718a1d247
dionnell <dionnellr1@gmail.com>
2025-07-23 22:13:35 -0400

    fix: ensure email field is stored in lowercase in Usuario schema

--- models/Usuario.js
+++ models/Usuario.js
@@ -10,7 +10,8 @@ const UsuarioSchema = mongoose.Schema({
         type: String,
         required: true,
         trim: true,
-        unique: true
+        unique: true,
+        lowercase: true
     },
     password: {
         type: String,
